ec5e0f6deddefbe393fb016ce34dceafeaa94432
JINHYUCK-r <wlsgur7701@naver.com>
2021-01-14 16:41:49 +0900

    OAuth2 를 이용한 구글로그인 셋팅

--- src/main/java/com/rjh/security1/config/SecurityConfig.java
+++ src/main/java/com/rjh/security1/config/SecurityConfig.java
@@ -29,9 +29,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest().permitAll()
 			.and()
 			.formLogin()
-			.loginPage("/loginForm")
+			.loginPage("/loginForm") //인증이 필요하면 이쪽으로 이동  
 			.loginProcessingUrl("/login") // /login주소가 호출되면 시큐리티가 낚아채서 대신 로그인을 진행해줌. 컨트롤러에 /login을 만들필요가 없음
-			.defaultSuccessUrl("/"); // 기본이동주소는 / 이지만 특정 주소를 요청해서 로그인하게 되면 그 주소로 바로 이동시켜 줌 
+			.defaultSuccessUrl("/") // 기본이동주소는 / 이지만 특정 주소를 요청해서 로그인하게 되면 그 주소로 바로 이동시켜 줌 
+			.and()
+			.oauth2Login()
+			.loginPage("/loginForm"); //구글 로그인창으로 이동됨 . 구글로그인이 완료된 뒤 후처리가 필요  
 	}
 	
 }
