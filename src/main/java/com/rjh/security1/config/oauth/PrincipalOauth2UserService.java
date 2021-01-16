69fc6c9073738deeae072dd3842fd9ca50c15b54
JINHYUCK-r <wlsgur7701@naver.com>
2021-01-16 15:07:43 +0900

    페이스북 로그인 구현

--- src/main/java/com/rjh/security1/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/rjh/security1/config/oauth/PrincipalOauth2UserService.java
@@ -10,6 +10,9 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import com.rjh.security1.config.auth.PrincipalDetails;
+import com.rjh.security1.config.oauth.provider.FacebookUserInfo;
+import com.rjh.security1.config.oauth.provider.GoogleUserInfo;
+import com.rjh.security1.config.oauth.provider.OAuth2UserInfo;
 import com.rjh.security1.model.User;
 import com.rjh.security1.repository.UserRepository;
 
@@ -37,12 +40,30 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService{
 		System.out.println("getAttributes : " + oauth2User.getAttributes()); //사용자의 정보가 담겨있음 
 		//sub : 구글의 프라이머리키 
 		
-		
-		String provider = userRequest.getClientRegistration().getClientId(); // google
-		String providerId = oauth2User.getAttribute("sub");
+		OAuth2UserInfo oAuth2UserInfo = null;
+		if(userRequest.getClientRegistration().getRegistrationId().equals("google")) {
+			System.out.println("구글 로그인 요청");
+			oAuth2UserInfo = new GoogleUserInfo(oauth2User.getAttributes()); //로그인 요청시 받은 정보를 우리가 만든 클래스에 담는다 
+		}else if(userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
+			System.out.println("페이스북 로그인 요청");
+			oAuth2UserInfo = new FacebookUserInfo(oauth2User.getAttributes()); 
+		}else {
+			System.out.println("우리는 구글과 페이스북만 지원해요");
+		}
+		/* 유저정보를 담는 클래스를 나누어서 만들지않고 구글만 했을 경우에는 이렇게받아도 된다.
+		String provider = userRequest.getClientRegistration().getRegistrationId(); // google
+		String providerId = oauth2User.getAttribute("sub"); //facebook일때는 sub값이 없기때문에 null이 됨 
 		String username = provider + "_" + providerId; //google_~~~~
 		String password = bCryptPasswordEncoder.encode("겟인데어");
 		String email = oauth2User.getAttribute("email");
+		String role = "ROLE_USER";
+		*/
+		
+		String provider = oAuth2UserInfo.getProvier();
+		String providerId = oAuth2UserInfo.getProviderId();
+		String username = provider + "_" + providerId; 
+		String password = bCryptPasswordEncoder.encode("겟인데어");
+		String email = oAuth2UserInfo.getEmail();
 		String role = "ROLE_USER";
 		
 		User userEntity = userRepository.findByUsername(username);
